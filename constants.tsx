6b007aefa73f426a346c66e1384b149937de3406
zarrarmalik78 <146096125+zarrarmalik78@users.noreply.github.com>
2026-01-18 05:22:01 +0500

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -19,7 +19,7 @@ export const COLORS = {
  */
 export const IMAGES = {
   hero: 'images/general/hero.png',
-  whyUs: './images/general/about.jpg',
+  whyUs: 'images/general/about.jpg',
   parentSaima: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=80&w=100&h=100',
   
   // Programs
